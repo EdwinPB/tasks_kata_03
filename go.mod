ac893d8243f00381bfdc681c2d91852ae137d977
EdwinPB <epolo@wawand.co>
2020-08-16 15:29:22 -0500

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module tasks_kata_03
+
+go 1.13
+
+require github.com/gobuffalo/buffalo-pop/v2 v2.2.0 // indirect
---
+++ grifts/init.go
@@ -0,0 +1,11 @@
+package grifts
+
+import (
+	"tasks_kata_03/actions"
+
+	"github.com/gobuffalo/buffalo"
+)
+
+func init() {
+	buffalo.Grifts(actions.App())
+}

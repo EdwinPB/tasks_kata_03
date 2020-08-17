dea1d2993fcc47801edd8172a3654483c2bab01b
EdwinPB <epolo@wawand.co>
2020-08-17 18:39:51 -0500

    removed unnecessary code

--- models/task.go
+++ models/task.go
@@ -1,11 +1,8 @@
 package models
 
 import (
-	"encoding/json"
 	"time"
 
-	"github.com/gobuffalo/pop"
-	"github.com/gobuffalo/validate"
 	"github.com/gofrs/uuid"
 )
 
@@ -20,35 +17,5 @@ type Task struct {
 	UpdatedAt      time.Time `json:"updated_at" db:"updated_at"`
 }
 
-// String is not required by pop and may be deleted
-func (t Task) String() string {
-	jt, _ := json.Marshal(t)
-	return string(jt)
-}
-
 // Tasks is not required by pop and may be deleted
 type Tasks []Task
-
-// String is not required by pop and may be deleted
-func (t Tasks) String() string {
-	jt, _ := json.Marshal(t)
-	return string(jt)
-}
-
-// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
-// This method is not required and may be deleted.
-func (t *Task) Validate(tx *pop.Connection) (*validate.Errors, error) {
-	return validate.NewErrors(), nil
-}
-
-// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
-// This method is not required and may be deleted.
-func (t *Task) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
-	return validate.NewErrors(), nil
-}
-
-// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
-// This method is not required and may be deleted.
-func (t *Task) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
-	return validate.NewErrors(), nil
-}
